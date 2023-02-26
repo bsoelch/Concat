088a1cda285438a041d256d2b2b2c17e68a65445
benjamin <benjamin.soelch@googlemail.com>
2023-02-26 15:59:55 +0100

    move tests files in subdirectories
    test fiels are now seperated by categories
    + avoid printing addresses, they are not reproducable

--- test.sh
+++ test.sh
@@ -1,12 +1,19 @@
 cArgs=( "-g" "-Wall" "-Wextra" "-Wshadow" "-Wold-style-definition" "-Wcast-qual" "-Werror" "-pedantic" )
 
 cd "./tests/"
+for d in */; do ##go through all directories in tests folder 
+cd "$d"
 for f in *.concat; do ## go through all concat files in the test directory
   echo "------------------------"
   echo "$f :"
-  touch "${f%.*}.in" # create input file if it does not exist
-  ../concat "$f" -o "${f%.*}.c" -q > "${f%.*}.out" 2> "${f%.*}.err" &&
-  gcc ${cArgs[@]} -Wno-unused "${f%.*}.c" -o "${f%.*}" >> "${f%.*}.out" 2>> "${f%.*}.err" &&
-  cat "${f%.*}.in" | "./${f%.*}" >> "${f%.*}.out" 2>> "${f%.*}.err"
-  ## TODO compare with expected output, print passed/failed
+  # create input & expected output files if they do not exist
+  touch "${f%.*}.in"
+  touch "${f%.*}.out"
+  touch "${f%.*}.err"
+  ../../concat "$f" -o "${f%.*}.c" -q > ".${f%.*}.out" 2> ".${f%.*}.err" &&
+  gcc ${cArgs[@]} -Wno-unused "${f%.*}.c" -o "${f%.*}" >> ".${f%.*}.out" 2>> ".${f%.*}.err" &&
+  cat "${f%.*}.in" | "./${f%.*}" >> ".${f%.*}.out" 2>> ".${f%.*}.err"
+  ## TODO compare output with expected output, print passed/failed remove output files
+done
+cd ..
 done
