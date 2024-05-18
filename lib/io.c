8be1c507bc1132c9195c8906f25399c14675983b
benjamin <benjamin.soelch@googlemail.com>
2024-05-18 21:46:17 +0200

    remove wrappers for FILE type
    now that the code does not need to be compiled as C source files
    it is possible to use an opaque struct pointer for the FILE type

--- lib/io.c
+++ lib/io.c
@@ -5,8 +5,6 @@
 #include <inttypes.h>
 #include <errno.h>
 
-typedef void concatIOHelper_File;
-
 typedef int32_t IOError;
 static const IOError FILE_ERR_NONE=0;
 static const IOError FILE_ERR_END_OF_FILE=-1;
@@ -19,14 +17,14 @@ static const IOError FILE_ERR_FILE_ALREADY_EXISTS=6;
 static const IOError FILE_ERR_ACCESS_DENIED=7;
 static const IOError FILE_ERR_INVALID_FILE=8;
 
-concatIOHelper_File* concatIOHelper_stdin(void){
-  return (concatIOHelper_File*)stdin;
+FILE* concatIOHelper_stdin(void){
+  return (FILE*)stdin;
 }
-concatIOHelper_File* concatIOHelper_stdout(void){
-  return (concatIOHelper_File*)stdout;
+FILE* concatIOHelper_stdout(void){
+  return (FILE*)stdout;
 }
-concatIOHelper_File* concatIOHelper_stderr(void){
-  return (concatIOHelper_File*)stderr;
+FILE* concatIOHelper_stderr(void){
+  return (FILE*)stderr;
 }
 void concatIOHelper_resetErrno(void){
   errno=0;
@@ -51,29 +49,3 @@ IOError concatIOHelper_checkErrno(void){
   }
   return FILE_ERR_IO;
 }
-// wrappers converting between FILE* and void*
-concatIOHelper_File* concatIOHelper_fopen(char const* path,char const* mode){
-  return (concatIOHelper_File*)fopen(path,mode);
-}
-void concatIOHelper_clearerr(concatIOHelper_File* f){
-  clearerr((FILE*)f);
-}
-int concatIOHelper_feof(concatIOHelper_File* f){
-  return feof((FILE*)f);
-}
-int concatIOHelper_ferror(concatIOHelper_File* f){
-  return ferror((FILE*)f);
-}
-size_t concatIOHelper_fread(void* buffer,size_t eltSize,size_t eltCount,concatIOHelper_File* f){
-  return fread(buffer,eltSize,eltCount,(FILE*)f);
-}
-size_t concatIOHelper_fwrite(void const* buffer,size_t eltSize,size_t eltCount,concatIOHelper_File* f){
-  return fwrite(buffer,eltSize,eltCount,(FILE*)f);
-}
-int concatIOHelper_fflush(concatIOHelper_File* f){
-  return fflush((FILE*)f);
-}
-int concatIOHelper_fclose(concatIOHelper_File* f){
-  return fclose((FILE*)f);
-}
-
