3fabc41d4694dc60d36e580ef03fb7908012aa57
benjamin <benjamin.soelch@googlemail.com>
2026-04-01 17:12:29 +0200

    code cleanup

--- test.py
+++ test.py
@@ -127,7 +127,7 @@ def runTests(path,*,cctFlags=None):
           futures.append(executor.submit(runTest,file,relPath,cctFlags=cctFlags))
       nTested += len(futures)
       for future in futures:
-        failedPath = future.result()  # Raises exception if occurred
+        failedPath = future.result()
         if failedPath is not None:
           failed.append(failedPath)
         else:
@@ -146,15 +146,14 @@ def runTests(path,*,cctFlags=None):
 def main():
   global concatPath
   global thread_count
-  ## XXX? should test-script clear console
+  ## TODO? should test-script clear console
   subprocess.run(["clear"])
-  parser = argparse.ArgumentParser(description="Concat Compiler Test")
+  parser = argparse.ArgumentParser()
   parser.add_argument('-X', action='store_true', help='Use developement version of compiler')
   parser.add_argument('--full', '-full', action='store_true', help='Include examples')
   parser.add_argument('-j', type=int, help='Number of threads, should be followed by a number', default=None)
   parser.add_argument('-cctArg', nargs=1, action = 'append', help='Add argument to concat compiler, can occur more than once', default=None)
-  args, unknown = parser.parse_known_args()
-  # Determine behavior based on flags
+  args, _ = parser.parse_known_args()
   concatPath = CURRENT_DIR + "/concat"
   if args.X:
     concatPath = CURRENT_DIR + "/concatX"
