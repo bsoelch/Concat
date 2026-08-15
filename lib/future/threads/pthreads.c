8b73468ea8040a57bf2fc4305b4b871181ffee0e
benjamin <benjamin.soelch@googlemail.com>
2026-08-15 12:50:51 +0200

    make pthreads wrapper compile

--- lib/future/threads/pthreads.c
+++ lib/future/threads/pthreads.c
@@ -2,15 +2,15 @@
 #include <pthread.h>
 typedef void* Thread_fun(void*);
 typedef uint64_t Thread_id;
-int cct_helper_createThread(Thread_id* thread_id,Thread_fun f,void* arg) {
+int cct_helper_createPThread(Thread_id* thread_id,Thread_fun f,void* arg) {
   pthread_t pthread_id;
   int res = pthread_create(&pthread_id,NULL,f,arg);
   *thread_id = pthread_id;
   return res;
 }
-int cct_helper_joinThread(Thread_id thread_id) {
+int cct_helper_joinPThread(Thread_id thread_id) {
   return pthread_join(thread_id,NULL);
 }
-void cct_helper_exitThread() {
+void cct_helper_exitPThread() {
   pthread_exit(NULL);
 }
